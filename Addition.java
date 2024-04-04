e7df8e914092ec3dad50626d43617af67b1138d3
neerajvanjare <neeraj.vanjare@gmail.com>
2024-04-04 11:58:52 +0530

    add addition file

---
+++ Addition.java
@@ -0,0 +1,13 @@
+public class Addition {
+     
+        static int a = 10;
+        static int b = 20;
+        
+
+  public static void main(String args[])
+  {
+        int c = a+b;
+     System.out.println("Addition :"+c);
+  }
+
+ }
