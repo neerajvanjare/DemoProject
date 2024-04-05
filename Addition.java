8d3031da7f82e4964806e0f63f502e3eb48f72b9
neerajvanjare <neeraj.vanjare@gmail.com>
2024-04-05 22:20:55 +0530

    write only addition in sop

--- Addition.java
+++ Addition.java
@@ -1,14 +1,9 @@
 public class Addition {
-     
         static int a = 10;
         static int b = 20;
-        
-
   public static void main(String args[])
   {
         int c = a+b;
-     System.out.println("Addition of a and b:"+c);
-
+     System.out.println("Addition : "+c);  
   }
-
  }
